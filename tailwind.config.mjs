32ad2f1e8ea321062113d84cc58257cf4c04802e
Mackey Guenther <mackeyguenther@gmail.com>
2025-02-04 16:02:05 -0800

    add keystatic

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				serif: ['Parclo Serif', 'serif'],
-				sans: ['Really Sans Small', 'sans-serif']
-			}
-		},
-	},
+	// theme: {
+	// 	extend: {
+	// 		fontFamily: {
+	// 			serif: ['Parclo Serif', 'serif'],
+	// 			sans: ['Really Sans Small', 'sans-serif']
+	// 		}
+	// 	},
+	// },
 	plugins: [
 		require('@tailwindcss/forms'),
 		require('tailwindcss-opentype'),
