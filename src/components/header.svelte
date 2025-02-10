35cfb9ce68686791d388f8b1e1d0127b5b0be3ed
moo <forrestbaum@gmail.com>
2025-02-09 23:04:57 -0800

    Update header.svelte
    Feb & March events header!

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -170,7 +170,7 @@
         <p class="text-sm leading-5 text-white text-center">
             <a href="#events">
                 <strong class="font-semibold ss-04"
-                    >🔔 October rides & events announced</strong
+                    >🔔 February & March events announced</strong
                 ><svg
                     viewBox="0 0 2 2"
                     class="mx-2 inline h-0.5 w-0.5 fill-current"
